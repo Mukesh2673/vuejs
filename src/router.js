54332896dedaee4047cfa937330824313998d72e
coopermukesh <coopermukesh@admins-Mac-mini.local>
2021-12-06 17:35:53 +0530

    add routing middleware in vuejs and use store to get value isloggedin:true

--- src/router.js
+++ src/router.js
@@ -1,22 +1,76 @@
-import cart from '../src/components/cart'
-import AddUser from './components/AddUser'
+
+import Login from './components/Login'
+import Dashboard from './components/Dashboard'
+import Movies from  './components/Movie'
+
+import guest from './router/middleware/guest'
+import auth from './router/middleware/auth'
+import isSubscribed  from './router/middleware/IsSubscribed'
+import {store} from './store/index'
+import middlewarePipeline from './router/middlewarePipeline'
 import {createRouter,createWebHistory} from 'vue-router'
 const routes=[
-    
-            {  
-            name:'AddUser',
-            component:AddUser,
-            path:'/addUser'
-            },
-            
             {
-            name:'cart',
-            component:cart,
-            path:'/cart'
-            },
-];
+                path:'/login',
+                name:'login',
+                component:Login,
+                meta:{
+                    middleware:[
+                        guest
+                    ]
+                }
+            }, 
+            {
+                path:'/dashboard',
+                name:'dashboard',
+                component:Dashboard,
+                meta:{
+                    middleware:[
+                        auth
+                    ]
+                },
+
+
+                children:[{
+                    path:'/dashboard/movies',
+                    name:'dashboard.movies',
+                    component:Movies,
+                    meta:{
+                        middleware:[
+                            auth,
+                            isSubscribed
+                        ]
+                    }
+
+                }],
+
+            }
+
+]
 const router=createRouter({
     history:createWebHistory(),
     routes
     });
+
+    router.beforeEach((to, from, next) => {
+    if (!to.meta.middleware) {
+        return next()
+    }
+    const middleware = to.meta.middleware
+
+    const context = {
+        to,
+        from,
+        next,
+        store
+    }
+
+
+    return middleware[0]({
+        ...context,
+        next: middlewarePipeline(context, middleware, 1)
+    })
+
+})
+
 export default router;
--- src/store/modules/user-module.js
+++ src/store/modules/user-module.js
@@ -1,57 +1,23 @@
-import axios from 'axios'
+
 const state={
     users:[],
-    count:0
+    count:0,
+    user:{
+        loggedIn:false,
+        isSubscribed:false
+    }
 
 };
 const getters={
-    usersList:state=>state.users,
-    counterList:state=>state.count
-};
-const actions={
-    async fetchUsers(context){
-        console.log(state.users);
-        const response=await axios.get("http://localhost:3000/users");
-        context.commit("setUsers",response.data)
-        
-    },
-    async addUsers({commit},user){
-        const response=await axios.post("http://localhost:3000/users",user);
-        commit("addNewUser",response.data)
-    
-    },
-    async deleteUser({commit},id){
-        await axios.delete(`http://localhost:3000/users/${id}`);
-        console.log(id);
-        commit("removeUser",id)
-     
-    },
-    addToCart({commit}){
-        console.log('hi');
-    commit("addcart")
-    }
-};
-const mutations={
-    setUsers:(state,users)=>{
-        state.users=users
-    },
 
-    addNewUser:(state,user)=>state.users.unshift(user),
-    removeUser:(state,id)=>{
-        
-       state.users.filter(user=>user.id !==id);
-        state.users.splice(user=>user.id,1);
-        
-    },
-    addcart:(state)=>{
-      
-       state.count=state.count+1;
-       
+    auth(state){
+        return state.user
     }
 };
+
+
+
 export default{
     state,
-    getters,
-    actions,
-    mutations
+    getters
 }
